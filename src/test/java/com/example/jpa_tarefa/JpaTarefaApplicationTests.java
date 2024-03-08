701573a173e503cbfb5206f43ada9741b1130c30
Gabriel <131317539+KnowledgeMoor@users.noreply.github.com>
2024-03-07 22:10:14 -0300

    Add files via upload

---
+++ src/test/java/com/example/jpa_tarefa/JpaTarefaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa_tarefa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTarefaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
